b39b9d437607dc06dd42170104abc20bdc436494
DuBois <Duende@MRD1.local>
2018-11-26 18:49:51 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,32 +1,23 @@
-// Create an array to store all cards
+
 var cards = ['queen', 'queen', 'king', 'king'];
 
-// Create an array to store the cards in play
 var cardsInPlay = [];
 
-// Create a variable cardOne to represent the first card the user flipped
-var cardOne = cards[0];
-// Add card to array of cards that are in play
-cardsInPlay.push(cardOne);
-// Use console.log() to display the card that the user flipped
-console.log("User flipped " + cardOne);
+var checkForMatch = function() {
+  	if (cardsInPlay[0] === cardsInPlay[1]) {
+    	console.log("You found a match!");
+  	} else {
+    	console.log("Sorry, try again.");
+    	}
+	};
 
-// Create a variable cardTwo to represent the second card the user flipped
-var cardTwo = cards[2];
-// Add card to array of cards that are in play
-cardsInPlay.push(cardTwo);
-// Use console.log() to display the card that the user flipped
-console.log("User flipped " + cardTwo);
+var flipCard = function(cardID) {
+	console.log("User flipped " + cards[cardID]);
+		cardsInPlay.push(cards[cardID]); {
+			checkForMatch();
+		}
+	};
 
+flipCard(0);
+flipCard(2);
 
-// Check to see if two cards have been played
-if (cardsInPlay.length === 2) {
-
-  // Check to see if two cards match and provide feedback to user
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.");
-
-  }
-}
